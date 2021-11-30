d535bbae8a88b2a31c5cb5baa0041ddab691b086
facelessmindz <fuzzzzzz@gmail.com>
2021-11-30 06:51:25 +0100

    - adding support for ZBrush 2022  - remove the version suffix "_202x" in data plugin directory.

---
+++ batch/upgrade-zCycler-plugin-2022.bat
@@ -0,0 +1,4 @@
+:: zCycler
+
+:: Upgrade zCycler in Zbrush 2020\ZStartup\Zplug64 directory
+xcopy /s /y "L:\programming\WORKSPACES\zbrush-zcycler\zCycler_2022.txt" "D:\Imaging\Pixologic\ZBrush 2022\ZStartup\ZPlugs64\" /f
